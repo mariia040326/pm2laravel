1040079c1ab95331a8a9dd0fac1130a9cf3a08ed
mariia040326 <holysh.mariia@student.uzhnu.edu.ua>
2022-11-07 13:18:37 +0200

    feat: add category factory & seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $outcomeCategories = ['Продукти',
+        'Комуналка',
+        'Розваги',
+        'Транспорт',
+        'Гігієна',
+        'Медицина',
+        'Одяг'
+    ];
+
+
+        return [
+            'name'=> fake()->unique()->randomElement($outcomeCategories),
+            'description'=>fake()->text(100),
+            'type'=>0
+        
+        ];
+    }
+    public function income()
+    {
+        $incomeCategories = ['Зарплата','Стипендія','Бабуся', 'Кешбек'];
+
+        return $this->state(function() use ($incomeCategories){
+            return [
+                'name'=> fake()->unique()->randomElement($incomeCategories),
+                'type'=>1
+            ];
+        });
+        
+    }
+}
